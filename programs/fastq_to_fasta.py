59d8ceae824c84f370481f473a84f928a13678bc
Alma Hurtig <alma.hurtig@hotmail.com>
2020-03-10 09:18:53 +0100

    Add fastq to fasta program.

---
+++ programs/fastq_to_fasta.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+'''
+Title: fasta_to_fastq.py
+Date: 2020-03-09
+Author: Alma Hurtig
+
+Description: This program convert a fastq-file to a fasta-file
+Procedure:
+1. Open the fastq input file and initiate an enumerator
+2. Print id-lines (exchange '@' to '>') and sequences to the output file
+
+
+Usage: ./fastq_to_fasta.py fastq_file output_fasta
+
+'''
+
+import sys
+
+with open (sys.argv[1], 'r') as fastq, open (sys.argv[2], 'w') as fasta:
+    fastq_read = fastq.readlines()
+    for no, line in enumerate(fastq_read):
+        line.rstrip()
+        if no % 4 == 0:
+            id_line = line.replace('@', '>', 1) #only first occurrance
+            fasta.write(id_line)
+        elif no % 4 == 1:
+            fasta.write(line)
