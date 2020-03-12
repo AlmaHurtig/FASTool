55a5ac0e156027806aac76aed56da7bc7e977ff3
Alma Hurtig <alma.hurtig@hotmail.com>
2020-03-12 11:20:30 +0100

    Change the range tuple in the for loops to be able to run the program no matter what lengths the input sequences have.

--- programs/translate_all_six_frames.py
+++ programs/translate_all_six_frames.py
@@ -6,12 +6,12 @@ Date: 2019-10-30
 Author: Alma Hurtig
 
 Description:
-    This program translate a given DNA sequence to protein sequence and print
+    This program translate a given nucleotide sequence to protein sequence and print
     all six possible reading frames.
 
 List of functions:
     'reverse': reverse a given sequence
-    'complement': create a complement of the given DNA sequence
+    'complement': create a complement of the given nucleotide sequence
     'reverse_complement': combine the two functions 'reverse' and 'complement'
     and print the reverse complement of the given DNA sequence
 
@@ -21,21 +21,21 @@ List of "non standard" modules:
 Procedure:
 1. Create a dictionary with codons as keys and their corresponding amino acid as
 value.
-2. Create a reverse complement of the given DNA sequence and save it to the
+2. Create a reverse complement of the given nucleotide sequence and save it to the
 variable 'reverse_strand'.
-3. Translate the DNA sequence and its reversed complement to protein sequences
+3. Translate the nucleotide sequence and its reversed complement to protein sequences
 by looping through the nucleotides and translate one codon at the time.
 4. Return all six frames to the console.
 
 Usage:
-./translate_all_six_frames.py DNA_sequence
+./translate_all_six_frames.py nucleotide_sequence
 
 '''
 
 import sys
 
 if len(sys.argv) != 2:
-    error_msg ='Exactly two input arguments are needed: Usage: ./project_work.py DNA_sequence'
+    error_msg ='Exactly two input arguments are needed: Usage: ./translate_all_six_frames.py nucleotide_sequence'
     print(error_msg, file = sys.stderr)
     sys.exit()
 
@@ -72,7 +72,7 @@ def reverse_complement(sequence):
     return(seq)
 
 reverse_strand = reverse_complement(sys.argv[1])
-print(reverse_strand)
+print('Reverse complement:', reverse_strand)
 
 def translate_nucleotides(sequence):
     protein_sequence_1 = ''
@@ -84,21 +84,21 @@ def translate_nucleotides(sequence):
             protein_sequence_1 += amino_acids[sequence[i:i+3]] # key = sequence[i:i+3] (nt + three following)
         if i > len(sequence):
             break
-    for i in range(1, len(sequence)-1, 3):
+    for i in range(1, len(sequence)-2, 3):
         if i < len(sequence):
             protein_sequence_2 += amino_acids[sequence[i:i+3]]
         if i > len(sequence):
             break
-    for i in range(2, len(sequence)-3, 3):
+    for i in range(2, len(sequence)-2, 3):
         if i < len(sequence):
             protein_sequence_3 += amino_acids[sequence[i:i+3]]
         elif i > len(sequence):
             break
     return('Frame 1: {} \nFrame 2: {} \nFrame 3: {}'.format(protein_sequence_1, protein_sequence_2, protein_sequence_3))
 
-DNA_seq = (sys.argv[1])
+nuc_seq = (sys.argv[1])
 
-translated_seq = translate_nucleotides(DNA_seq)
+translated_seq = translate_nucleotides(nuc_seq)
 print('Forward: \n{}'.format(translated_seq))
 
 translated_seq_rev = translate_nucleotides(reverse_strand)
